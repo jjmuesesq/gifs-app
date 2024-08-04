8cc2ba532abf4dbb5faf2600cf60653c53afbc33
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-03 21:31:48 -0500

    :sparkles: @ViewChild - Referencia al HTML

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'gifs-search-box',
     template: `
         <h5>Buscar</h5>
-        <input type="text" class="form-control" placeholder="Buscar gifs...">
+        <input type="text" 
+               class="form-control" 
+               placeholder="Buscar gifs..."
+               (keyup.enter)="searchTag(  )"
+               #txtTagInput
+        >
     `
 })
 
 export class SearchBoxComponent {
+    @ViewChild( 'txtTagInput' ) // nos sirve para tomar una referencia local de un elemento html
+    public tagInput!: ElementRef<HTMLInputElement>; //siempre va a estar ese valor.
+
     constructor() { }
+
+    // searchTag( newTag: string){  forma 1 (keyup.enter)="searchTag( txtTagInput.value )
+    //     console.log({ newTag });
+    // }
+
+    searchTag(){
+        const newTag = this.tagInput.nativeElement.value;
+        console.log({ newTag });
+    }
 }
