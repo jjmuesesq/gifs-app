f0c4ebf257dde09d2f4ed5ae4667aa2a2a734bec
jjmuesesq <jjmuesesq@unal.edu.co>
2024-08-05 13:12:52 -0500

    :sparkles: Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -11,10 +11,21 @@ export class GifsService {
         return [...this._tagsHistory];
     }
     
+    //organizar
+    private organizeHistory(tag: string){
+        tag = tag.toLowerCase(); //se maneja todo en minuscula
+        if(this._tagsHistory.includes( tag )){
+            this._tagsHistory = this._tagsHistory.filter( (oldTag ) => oldTag !== tag ); //retorna los elementos sin el tag que coincide ne la lista
+        }
+
+        this._tagsHistory.unshift(tag); //inserto el tag al inicio
+        this._tagsHistory = this._tagsHistory.splice(0, 10); //mostrar solo 10 elementos
+    }
 
     //añadir un tag al inicio del arreglo
     searchTag( tag: string ):void {
-        this._tagsHistory.unshift(tag);
+        if(tag.length === 0 ) return;
+        this.organizeHistory(tag);
         console.log(this.tagsHistory);
     }
 }
